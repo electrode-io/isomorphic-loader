86c67940bec45ca4f05b191c5d11434d75703cdb
Joel Chen <xchen@walmartlabs.com>
2018-03-27 13:16:41 -0700

    update to use electrode archetype njs module

--- test/spec/isomorphic.dev.spec.js
+++ test/spec/isomorphic.dev.spec.js
@@ -2,24 +2,24 @@
 
 /* eslint-disable max-nested-callbacks */
 
-var fs = require("fs");
-var Path = require("path");
-var rimraf = require("rimraf");
-var chai = require("chai");
-var clone = require("clone");
-var deepExtend = require("deep-extend");
-var fetchUrl = require("fetch").fetchUrl;
+const fs = require("fs");
+const Path = require("path");
+const rimraf = require("rimraf");
+const chai = require("chai");
+const clone = require("clone");
+const deepExtend = require("deep-extend");
+const fetchUrl = require("fetch").fetchUrl;
 
-var webpack = require("webpack");
-var WebpackDevServer = require("webpack-dev-server");
-var webpackConfig = clone(require("../webpack.config"));
-var Config = require("../../lib/config");
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const webpackConfig = clone(require("../webpack.config"));
+const Config = require("../../lib/config");
 
-var expect = chai.expect;
+const expect = chai.expect;
 
-var extendRequire = require("../../lib/extend-require");
-var IsomorphicLoaderPlugin = require("../../lib/webpack-plugin");
-var logger = require("../../lib/logger");
+const extendRequire = require("../../lib/extend-require");
+const IsomorphicLoaderPlugin = require("../../lib/webpack-plugin");
+const logger = require("../../lib/logger");
 
 webpackConfig.output.path = "/";
 
@@ -27,7 +27,7 @@ describe("isomorphic extend with webpack-dev-server", function() {
   this.timeout(4000);
   Config.initialWaitingNoticeDelay = 0;
 
-  var configFile = Path.resolve(Config.configFile);
+  const configFile = Path.resolve(Config.configFile);
 
   function writeFont(data) {
     fs.writeFileSync(Path.resolve("test/client/fonts/font.ttf"), data || "ttfttfttf\nfontfontfont");
@@ -38,17 +38,17 @@ describe("isomorphic extend with webpack-dev-server", function() {
     rimraf.sync(configFile);
   }
 
-  var webpackDevServer;
+  let webpackDevServer;
 
   function start(config, devConfig, callback) {
-    var compiler = webpack(config);
+    const compiler = webpack(config);
     webpackDevServer = new WebpackDevServer(compiler, deepExtend({}, devConfig));
     webpackDevServer.listen(8080, "localhost", function() {
       callback();
     });
   }
 
-  var devConfig = {
+  const devConfig = {
     host: "localhost",
     port: 8080,
     publicPath: webpackConfig.output.publicPath,
@@ -81,8 +81,8 @@ describe("isomorphic extend with webpack-dev-server", function() {
 
   before(cleanup);
 
-  var origLog = logger.log;
-  var logs = [];
+  const origLog = logger.log;
+  let logs = [];
   beforeEach(function() {
     Config.verbose = true;
     Config.reloadDelay = 10;
@@ -101,7 +101,7 @@ describe("isomorphic extend with webpack-dev-server", function() {
   function verifyRemoteAssets(fontHash, callback) {
     fetchUrl("http://localhost:8080/test/isomorphic-assets.json", function(err, meta, body) {
       expect(meta.status).to.equal(200);
-      var assets = JSON.parse(body.toString());
+      const assets = JSON.parse(body.toString());
       expect(assets.marked["test/client/fonts/font.ttf"]).to.equal(fontHash);
       callback();
     });
@@ -140,19 +140,19 @@ describe("isomorphic extend with webpack-dev-server", function() {
   }
 
   function verifyRenameEvent(callback) {
-    var newName = Path.resolve(".iso-config.json");
-    var renames = [{ src: configFile, dst: newName }, { src: newName, dst: configFile }];
+    const newName = Path.resolve(".iso-config.json");
+    const renames = [{ src: configFile, dst: newName }, { src: newName, dst: configFile }];
 
-    var check = function() {
-      var found = logs.find(function(x) {
+    const check = function() {
+      const found = logs.find(function(x) {
         return x.indexOf("unexpected config file watch event rename") > 0;
       });
-      var x = renames.shift();
+      const x = renames.shift();
       if (!x) {
         expect(found).to.be.a.string;
         return callback();
       }
-      fs.rename(x.src, x.dst, function(err) {
+      return fs.rename(x.src, x.dst, function(err) {
         expect(err).to.equal(null);
         setTimeout(check, 10);
       });
@@ -163,7 +163,7 @@ describe("isomorphic extend with webpack-dev-server", function() {
 
   function verifyBadConfig(callback) {
     function check() {
-      var found = logs.find(function(x) {
+      const found = logs.find(function(x) {
         return x.indexOf("extend require: file watcher load assets error") >= 0;
       });
       logs = [];
@@ -181,7 +181,7 @@ describe("isomorphic extend with webpack-dev-server", function() {
 
   function verifySkipReload(callback) {
     function check() {
-      var found = logs.find(function(x) {
+      const found = logs.find(function(x) {
         return x.indexOf("config is now VALID - skip reload. timestamp did not change") >= 0;
       });
       logs = [];
@@ -192,7 +192,7 @@ describe("isomorphic extend with webpack-dev-server", function() {
       }
     }
 
-    var isoConfig = JSON.parse(fs.readFileSync(configFile));
+    const isoConfig = JSON.parse(fs.readFileSync(configFile));
     isoConfig.timestamp -= 10000;
     fs.writeFileSync(configFile, JSON.stringify(isoConfig, null, 2));
     check();
@@ -204,9 +204,9 @@ describe("isomorphic extend with webpack-dev-server", function() {
   });
 
   it("should start and watch for file change event", function(done) {
-    test(clone(webpackConfig), function() {
-      verifyRenameEvent(function() {
-        setTimeout(function() {
+    test(clone(webpackConfig), () => {
+      verifyRenameEvent(() => {
+        setTimeout(() => {
           extendRequire.deactivate();
           stop(done);
         }, 10);
@@ -215,7 +215,7 @@ describe("isomorphic extend with webpack-dev-server", function() {
   });
 
   function testAddUrl(publicPath, skipSetEnv, done) {
-    var wpConfig = clone(webpackConfig);
+    const wpConfig = clone(webpackConfig);
     wpConfig.output.publicPath = publicPath;
     wpConfig.plugins = [
       new IsomorphicLoaderPlugin({
@@ -223,9 +223,9 @@ describe("isomorphic extend with webpack-dev-server", function() {
       })
     ];
     test(wpConfig, function() {
-      var font = require("../client/fonts/font.ttf");
+      const font = require("../client/fonts/font.ttf");
       expect(font).to.equal("http://localhost:8080/test/1e2bf10d5113abdb2ca03d0d0f4f7dd1.ttf");
-      var env = skipSetEnv ? (!!process.env.WEBPACK_DEV).toString() : process.env.WEBPACK_DEV;
+      const env = skipSetEnv ? (!!process.env.WEBPACK_DEV).toString() : process.env.WEBPACK_DEV;
       expect(env).to.equal((!skipSetEnv).toString());
       setTimeout(function() {
         verifySkipReload(function() {
