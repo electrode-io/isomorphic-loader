3548b1f40f60475b51047fb5147640f393f0953e
Joel Chen <joel123@gmail.com>
2019-06-04 12:20:44 -0700

    use lockfile to sync writing config file

--- test/spec/isomorphic.dev.spec.js
+++ test/spec/isomorphic.dev.spec.js
@@ -1,40 +1,23 @@
 "use strict";
 
 const clone = require("clone");
+const webpackInfo = require("../lib/webpack-info");
 
-const testInfo = {
-  v3: {
-    skip: false,
-    webpack: "webpack@3.x.x",
-    devServer: "webpack-dev-server@2.x.x",
-    config: "../webpack.config"
-  },
-  v4: {
-    skip: false,
-    webpack: "webpack@4.x.x",
-    devServer: "webpack-dev-server@3.x.x",
-    config: "../webpack4.config"
-  }
-};
-
-Object.keys(testInfo).forEach(ver => {
-  const info = testInfo[ver];
+Object.keys(webpackInfo).forEach(ver => {
+  const info = webpackInfo[ver];
   if (info.skip) return;
-  describe(`isomorphic extend with webpack ${ver} & webpack-dev-server`, function() {
-    this.timeout(10000);
-    /*
-     * This usage of require depends on fyn and flat-module
-     */
 
-    const webpack = require(info.webpack);
-    const WebpackDevServer = require(info.devServer);
-    const webpackConfig = clone(require(info.config));
+  const xrequire = info.xrequire;
+  const webpack = xrequire(info.webpack);
+  const WebpackDevServer = xrequire(info.devServer);
+  const webpackConfig = clone(xrequire(info.config));
 
-    require("../lib/isomorphic.dev.spec")({
-      tag: `webpack_${ver}`,
-      webpack,
-      WebpackDevServer,
-      webpackConfig
-    });
+  require("../lib/isomorphic.dev.spec")({
+    title: `isomorphic extend with webpack ${ver} & webpack-dev-server`,
+    tag: `webpack_${ver}`,
+    timeout: 20000,
+    webpack,
+    WebpackDevServer,
+    webpackConfig
   });
 });
