8eb0a6e2b2b36ebfbc29a172dfaea54cc8fb35dd
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-09-15 16:31:22 -0700

    fix cdn mapping based on feedback

---
+++ test/spec/extended-require-cdn-mapping.spec.js
@@ -0,0 +1,17 @@
+const _checkCdnMapping = require("../../lib/extend-require")._checkCdnMapping;
+
+describe("cdnMapping", () => {
+    it("maps asset files to cdn addresses'", () => {
+        const assetUrl = _checkCdnMapping({
+            cdnUrlMappingFile: "test/client/cdnMapping.json"
+        }, "index.js");
+        expect(assetUrl).to.equal("http://cdn.localhost.com/index.js");
+    });
+
+    it("or return original asset filename", () => {
+        const assetUrl = _checkCdnMapping({
+            cdnUrlMappingFile: "test/client/cdnMapping.json"
+        }, "index2.js");
+        expect(assetUrl).to.equal("index2.js");
+    });
+});
